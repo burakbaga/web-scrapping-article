a6d9be2d366ab175eeba732c92394d0a2c878618
burakbaga <bagataburak@gmail.com>
2019-06-18 13:39:02 +0300

    s

--- myvenv/Scripts/activate.bat
+++ myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\bagat\OneDrive\Masaüstü\kosemsite\myvenv"
+set "VIRTUAL_ENV=C:\Users\bagat\OneDrive\Documents\GitHub\web-scrapping-article\myvenv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
